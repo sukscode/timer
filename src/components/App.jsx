576e551f6f32dd179845fb58998e6594f60486ee
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-21 12:57:16 +0530

    timer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  setInterval(updateTime, 1000);
 
-  function increase() {
-    setCount(count + 1);
-  }
+  const now = new Date().toLocaleTimeString();
 
-  function decrease() {
-    setCount(count - 1);
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
 
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={decrease}>-</button>
-      <button onClick={increase}>+</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
